75484d3b3283111a51736b3d00919ae486db4665
bharatayasa <bharatayasa40@gmail.com>
2024-07-08 21:44:16 +0800

    fix file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['Authorization'];
+    const token = req.headers['authorization'];
     if (!token) return res.status(401).json({
         message: 'Unauthenticated.'
     });
